17659e8515b21aee05725d6009ad3b5d259acce2
devMachine <devmachine@devMachines-MBP.fios-router.home>
2016-04-28 20:56:13 -0400

    update to app.js

--- app.js
+++ app.js
@@ -1,26 +1,85 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var exhbs = require('express-handlebars');
+var flash = require('connect-flash');
+var session = require('express-session');
+var expressValidator = require('express-validator');
+var passport = require('passport');
+var LocalStrategy = require('passport-local'), Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/passport_example');
+//var favicon = require('serve-favicon');
+//var logger = require('morgan');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+
+//init app
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('handlebars', exhbs({defaultLayout: 'layout'}));
+app.set('view engine', 'handlebars');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+//express session
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+
+//Passport Init
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+//Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value){
+    var namespace = param.split('.'),
+    root = namespace.shift(),
+    formParam = root;
+
+    while(namespace.length){
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return{
+      param : formParam,
+      msg : msg,
+      value : value
+    };
+  }
+}));
+
+
+// Connect flash
+app.use(flash());
+
+
+//Global Vars
+app.use(function (req, res, next){
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+
 
 app.use('/', routes);
 app.use('/users', users);
@@ -31,6 +90,8 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
+
 
 // error handlers
 
